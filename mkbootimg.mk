4534f53b1390b68ddfac3e73217c412b7f03dc15
AnthonyKwon <12590757+AnthonyKwon@users.noreply.github.com>
2020-03-26 18:39:02 +0900

    Removed SELinux enforcement

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -9,7 +9,6 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES)
 	$(call pretty,"Target boot image: $@")
 	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE))
-	@echo "SEANDROIDENFORCE" >> $@
 	@echo "Made boot image: $@"
 
 ## Overload recoveryimg generation: Same as the original, + BUMP
@@ -17,5 +16,4 @@ $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(recovery_ramdisk) $(recovery_k
 	$(call build-recoveryimage-target, $@)
 	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE))
-	@echo "SEANDROIDENFORCE" >> $@
 @echo "Made recovery image: $@"
