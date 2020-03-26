c89de18e3e4b47214a977014c14227dd53d3c543
AnthonyKwon <12590757+AnthonyKwon@users.noreply.github.com>
2020-03-26 17:40:57 +0900

    Added missing line in fstab (This will fix build error)

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -25,6 +25,14 @@ LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.recovery.universal3475.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.recovery.universal3475.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.universal3475.usb.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
